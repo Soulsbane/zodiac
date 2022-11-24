3b96a20c50d85497215fceb4fa3b464bee9d59f3
Paul Crane <paul@acheronsoft.com>
2022-11-23 23:20:28 -0600

    More reworking of variable names

--- cmd/zodiac/main.go
+++ cmd/zodiac/main.go
@@ -10,14 +10,14 @@ import (
 
 func main() {
 
-	for _, yr := range []int{1935, 1938, 1968, 1972, 1976} {
+	for _, yr := range []int{1935, 1938, 1968, 1972, 1976, 1984, 2017} {
 		sign := zodiac.GetSign(yr)
-		fmt.Printf("%d: %s %s, %s, Cycle year %d %s\n",
-			sign.Year, sign.Animal, sign.YinYang, sign.Element, sign.Year, sign.StemBranch)
+		fmt.Printf("%d: %s %s, %s, Cycle year %d %s %s\n",
+			sign.Year, sign.Animal, sign.YinYang, sign.Element, sign.Year, sign.Stem, sign.Branch)
 	}
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
 	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	// w.ShowAndRun()
 }
--- internal/zodiac/zodiac.go
+++ internal/zodiac/zodiac.go
@@ -1,20 +1,21 @@
 package zodiac
 
 var (
-	animalString = []string{"Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
-		"Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"}
-	stemYYString  = []string{"Yang", "Yin"}
-	elementString = []string{"Wood", "Fire", "Earth", "Metal", "Water"}
-	stems         = []rune("甲乙丙丁戊己庚辛壬癸")
-	branches      = []rune("子丑寅卯辰巳午未申酉戌亥")
+	animals = []string{"Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse",
+		"Goat", "Monkey", "Rooster", "Dog", "Pig"}
+	YinYang  = []string{"Yang", "Yin"}
+	elements = []string{"Wood", "Fire", "Earth", "Metal", "Water"}
+	stems    = []rune("甲乙丙丁戊己庚辛壬癸")
+	branches = []rune("子丑寅卯辰巳午未申酉戌亥")
 )
 
 type Sign struct {
-	Animal     string
-	YinYang    string
-	Element    string
-	StemBranch string
-	Year       int
+	Animal  string
+	YinYang string
+	Element string
+	Stem    string
+	Branch  string
+	Year    int
 }
 
 func GetSign(year int) Sign {
@@ -24,10 +25,11 @@ func GetSign(year int) Sign {
 	stem := year % 10
 	branch := year % 12
 
-	sign.Animal = animalString[branch]
-	sign.YinYang = stemYYString[stem%2]
-	sign.Element = elementString[stem/2]
-	sign.StemBranch = string([]rune{stems[stem], branches[branch]})
+	sign.Animal = animals[branch]
+	sign.YinYang = YinYang[stem%2]
+	sign.Element = elements[stem/2]
+	sign.Stem = string(stems[stem])
+	sign.Branch = string(branches[branch])
 	sign.Year = year%60 + 1
 
 	return sign
